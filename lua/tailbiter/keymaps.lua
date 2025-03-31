8d864aa74024fb7a9614a7e39ccbc54ab630142d
Лаптев Матвей Дмитриевич <mlaptev@elvees.com>
2025-03-31 20:16:42 +0300

    31_03_2025

--- lua/tailbiter/keymaps.lua
+++ lua/tailbiter/keymaps.lua
@@ -6,8 +6,15 @@ map("n", '<C-q>', '<esc>:q<cr>')
 
 map("n", '<C-y>', ':tabnew ')
 
+map("n", '<C-Left>', 'gT')
+
+map("n", '<C-Right>', 'gt')
+
 map("i", '<C-s>', '<esc>:w<cr>')
 
 map("i", '<C-y>', '<esc>:tabnew ')
 
+map("i", '<C-Left>', '<esc>gT')
+
+map("i", '<C-Right>', '<esc>gt')
 
